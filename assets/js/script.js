bd6bcd1304a00078cd4f791b4c07b42540b075ab
Erin Holmes <e.holmes09@yahoo.com>
2021-05-26 10:24:49 -0500

    added status columns and set task id counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+var taskIdCounter = 0;
+
 var taskFormHandler = function(event) {
     event.preventDefault();
 
@@ -27,6 +29,9 @@ var createTaskEl = function(taskDataObj) {
     //create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
+
+    //add task id as a custom attribute
+    listItemEl.setAttribute("data-task-id", taskIdCounter);
     
     //create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
@@ -38,6 +43,8 @@ var createTaskEl = function(taskDataObj) {
 
     //add list item to ul 
     tasksToDoEl.appendChild(listItemEl);
+
+    taskIdCounter++;
 }
 
 formEl.addEventListener("submit", taskFormHandler);
